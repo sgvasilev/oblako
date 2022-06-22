738f86825bf83e26acabf34b7cbe8f3c00f24910
Sergey <sendto.vasilevsg@gmail.com>
2022-06-22 10:14:39 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://oblako-client.herokuapp.com/"
+    origins "oblako-client.herokuapp.com"
 
     resource "*",
       headers: :any,
